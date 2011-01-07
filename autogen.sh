57f03fd843c6c455f2fa575c0823d69bb136cf9f
Uwe Hermann <uwe@hermann-uwe.de>
2011-01-07 18:00:42 +0100

    autogen.sh: Bugfix for MinGW/Windows.

--- autogen.sh
+++ autogen.sh
@@ -19,15 +19,19 @@
 ## 
 
 OS=`uname`
+
+LIBTOOLIZE=libtoolize
+ACLOCAL_DIR=
+
 if [ "x$OS" = "xDarwin" ]; then
-    LIBTOOLIZE=glibtoolize
-else
-    LIBTOOLIZE=libtoolize
+	LIBTOOLIZE=glibtoolize
+elif [ "x$OS" = "xMINGW32_NT-5.1" ]; then
+	ACLOCAL_DIR="-I /usr/local/share/aclocal"
 fi
 
 echo "Generating build system..."
 ${LIBTOOLIZE} --install --copy --quiet || exit 1
-aclocal || exit 1
+aclocal ${ACLOCAL_DIR} || exit 1
 autoheader || exit 1
 automake --add-missing --copy --foreign || exit 1
 autoconf || exit 1
