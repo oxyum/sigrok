c287ba05f74d636206b122560c68f4b656c31fa2
Gareth McMullin <gareth@blacksphere.co.nz>
2011-11-12 23:31:47 +1300

    Added device selection to the menu.
    There's still a bug in that the menu new menu items don't appear after
    the a rescan.

--- gtk/toolbar.c
+++ gtk/toolbar.c
@@ -29,13 +29,17 @@ static void dev_selected(GtkComboBox *dev, GObject *parent)
 	GtkTreeModel *devlist = gtk_combo_box_get_model(dev);
 	GtkTreeIter iter;
 	const gchar *name;
+	GtkCheckMenuItem *menuitem;
 	struct sr_device *device;
 
 	if(!gtk_combo_box_get_active_iter(dev, &iter)) {
 		g_object_set_data(parent, "device", NULL);
 		return;
 	}
-	gtk_tree_model_get(devlist, &iter, 0, &name, 1, &device, -1);
+	gtk_tree_model_get(devlist, &iter, 0, &name, 1, &device,
+				2, &menuitem, -1);
+
+	gtk_check_menu_item_set_active(menuitem, TRUE);
 
 	sr_session_device_clear();
 	if (sr_session_device_add(device) != SR_OK) {
@@ -239,6 +243,23 @@ static void dev_set_probes(GtkAction *action, GtkWindow *parent)
 
 	gtk_widget_destroy(dialog);
 }
+static void dev_menuitem_toggled(GtkMenuItem *item, GtkComboBox *combo)
+{
+	GtkTreeModel *model = gtk_combo_box_get_model(combo);
+	GtkTreeIter iter;
+
+	if (!gtk_check_menu_item_get_active(GTK_CHECK_MENU_ITEM(item)))
+		return;
+	
+	if (gtk_tree_model_get_iter_first(model, &iter)) do {
+		gchar *name;
+		gtk_tree_model_get(model, &iter, 0, &name, -1);
+		if (g_str_equal(name, gtk_menu_item_get_label(item))) {
+			gtk_combo_box_set_active_iter(combo, &iter);
+			return;
+		}
+	} while(gtk_tree_model_iter_next(model, &iter));
+}
 
 #define GET_DEVICE_INSTANCE(device) \
 	(device)->plugin->get_device_info((device)->plugin_index, \
@@ -254,6 +275,11 @@ static void dev_list_refresh(GtkAction *action, gpointer data)
 	struct sr_device_instance *sdi;
 	gchar *sdevname = NULL;
 	GSList *devices, *l;
+	GtkUIManager *ui = g_object_get_data(data, "ui_manager");
+	GtkWidget *menuitem = gtk_ui_manager_get_widget(ui,
+					"/menubar/DevMenu/DevSelectMenu");
+	GtkMenuShell *devmenu = GTK_MENU_SHELL(gtk_menu_item_get_submenu(GTK_MENU_ITEM(menuitem)));
+	GSList *radiolist = NULL;
 
 	/* Make a copy of the selected device's short name for comparison.
 	 * We wish to select the same device after the refresh if possible.
@@ -264,6 +290,13 @@ static void dev_list_refresh(GtkAction *action, gpointer data)
 		sdevname = g_strdup(device->plugin->name);
 	}
 
+	/* Destroy the old menu items */
+	if (gtk_tree_model_get_iter_first(GTK_TREE_MODEL(devlist), &iter)) do {
+		GtkMenuItem *item;
+		gtk_tree_model_get(GTK_TREE_MODEL(devlist), &iter, 2, &item, -1);
+		gtk_object_destroy(GTK_OBJECT(item));
+	} while(gtk_tree_model_iter_next(GTK_TREE_MODEL(devlist), &iter));
+
 	gtk_list_store_clear(devlist);
 
 	/* Scan for new devices and update our list */
@@ -273,11 +306,23 @@ static void dev_list_refresh(GtkAction *action, gpointer data)
 	for (l = devices; l; l = l->next) {
 		device = l->data;
 		sdi = GET_DEVICE_INSTANCE(device);
+		gchar *name = sdi->model ? sdi->model : sdi->vendor;
+
+		menuitem = gtk_radio_menu_item_new_with_label(radiolist, name);
+		if(!radiolist)
+			radiolist = gtk_radio_menu_item_get_group(
+					GTK_RADIO_MENU_ITEM(menuitem));
+		g_signal_connect(menuitem, "toggled",
+				G_CALLBACK(dev_menuitem_toggled), dev);
+		gtk_menu_shell_prepend(devmenu, menuitem);
+
 		gtk_list_store_append(devlist, &iter);
 		gtk_list_store_set(devlist, &iter,
-				0, sdi->model ? sdi->model : sdi->vendor,
+				0, name,
 				1, device,
+				2, menuitem,
 				-1);
+
 		if (sdevname && g_str_equal(sdevname, device->plugin->name))
 			gtk_combo_box_set_active_iter(dev, &iter);
 	}
@@ -393,6 +438,7 @@ void help_about(GtkAction *action, GtkWindow *parent);
 static const GtkActionEntry action_items[] = {
 	/* name, stock-id, label, accel, tooltip, callback */
 	{"DevMenu", NULL, "_Device", NULL, NULL, NULL},
+	{"DevSelectMenu", NULL, "Select Device", NULL, NULL, NULL},
 	{"DevRescan", GTK_STOCK_REFRESH, "_Rescan", "<control>R",
 		"Rescan for LA devices", G_CALLBACK(dev_list_refresh)},
 	{"DevProperties", GTK_STOCK_PROPERTIES, "_Properties", "<control>P",
@@ -427,7 +473,10 @@ static const char ui_xml[] =
 "<ui>"
 "  <menubar>"
 "    <menu action='DevMenu'>"
-"      <menuitem action='DevRescan'/>"
+"      <menu action='DevSelectMenu'>"
+"        <separator/>"
+"        <menuitem action='DevRescan'/>"
+"      </menu>"
 "      <menuitem action='DevProperties'/>"
 "      <menuitem action='DevProbes'/>"
 "      <separator/>"
@@ -472,6 +521,7 @@ GtkWidget *toolbar_init(GtkWindow *parent)
 					G_N_ELEMENTS(toggle_items), parent);
 
 	GtkUIManager *ui = gtk_ui_manager_new();
+	g_object_set_data(G_OBJECT(parent), "ui_manager", ui);
 	gtk_ui_manager_insert_action_group(ui, ag, 0);
 	GtkAccelGroup *accel = gtk_ui_manager_get_accel_group(ui);
 	gtk_window_add_accel_group(parent, accel);
@@ -496,8 +546,8 @@ GtkWidget *toolbar_init(GtkWindow *parent)
 	gtk_toolbar_insert(toolbar, toolitem, 0);
 
 	/* Populate device list */
-	GtkListStore *devlist = gtk_list_store_new(2,
-			G_TYPE_STRING, G_TYPE_POINTER);
+	GtkListStore *devlist = gtk_list_store_new(3,
+			G_TYPE_STRING, G_TYPE_POINTER, GTK_TYPE_CHECK_MENU_ITEM);
 	GtkCellRenderer *cel = gtk_cell_renderer_text_new();
 	gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(dev), cel, TRUE);
 	gtk_cell_layout_add_attribute(GTK_CELL_LAYOUT(dev), cel, "text", 0);
